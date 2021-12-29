eee90da84f3c38e60722c088393cb4436b37a6a0
Hugo López-Fernández <hlfernandez@uvigo.es>
2021-12-29 17:26:14 +0100

    Improves error checking

--- resources/working_dir/run.sh
+++ resources/working_dir/run.sh
@@ -6,14 +6,20 @@ PROJECT_DIR=$1
 PARAMS_FILE="cga.params"
 ADDITIONAL_COMPI_PARAMS=$2
 
+show_error() {
+	tput setaf 1
+	echo -e "${1}"
+	tput sgr0
+}
+
 if [[ ! -f "${PROJECT_DIR}/${PARAMS_FILE}" ]]; then
-	echo "The parameters file (${PROJECT_DIR}/${PARAMS_FILE}) does not exist.";
-	exit;
+	show_error "The parameters file (${PROJECT_DIR}/${PARAMS_FILE}) does not exist."
+	exit 1
 else
 	check=$(cat ${PROJECT_DIR}/${PARAMS_FILE} | grep "host_working_dir\=/path/to/host/working/dir" | wc -l)
 	if [ check -eq 1 ]; then
-		echo "It seems that host_working_dir is not set in the params file (${PROJECT_DIR}/${PARAMS_FILE})";
-		exit;	
+		show_error "It seems that host_working_dir is not set in the params file (${PROJECT_DIR}/${PARAMS_FILE})"
+		exit 1
 	fi
 fi
 
